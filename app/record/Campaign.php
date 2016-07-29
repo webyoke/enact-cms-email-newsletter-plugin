115278dc97e9662465b9641c117bb1ba502f73bf
jbhamilton <brad@bradleyhamilton.com>
2016-07-29 18:55:30 -0400

    updated record paradigm

--- app/record/Campaign.php
+++ app/record/Campaign.php
@@ -6,172 +6,71 @@ class Campaign extends \Disco\classes\Record {
     protected $model = '\EmailNewsletter\model\Campaign';
     
     
+
     /**
-    * @var Array $validators The validation rules for the fields.
+     * @var null|string $autoIncrementField The autoincrement field name.
     */
-    public static $validators = null;
-
-
-    public static function autoIncrementField(){
-        return 'id';
-    }//autoIncrementField
-
-
-    /**
-     * Get the fields of the record.
-     *
-     *
-     * @return array The fields.
-     */
-    public static function fieldNames(){
-        return Array(
-    
-                'id',
-                'subject',
-                'template',
-                'token',
-                'total_sent',
-                'total_viewed',
-                'total_click_through',
-                'created_on',
-                'variables'
-        );
-    }//fields
-
-
-
-    /**
-     * Get the fields of the record. (Wrapper for `self::fields()`).
-     *
-     *
-     * @return array The fields.
-     */
-    public function getFieldNames(){
-        return self::fieldNames();
-    }//getFields
-
-
-
-    /**
-     * Get the required fields of the record (cannot be NULL).
-     *
-     *
-     * @return array The fields.
-     */
-    public static function requiredFieldNames(){
-        return Array(
-    
-                    'subject',
-                    'template',
-                    'token',
-                    'total_sent',
-                    'total_viewed',
-                    'total_click_through',
-                    'created_on'
-        );
-    }//requiredFields
-
-
+    protected $fieldDefinitions = array (
+  'id' => 
+  array (
+    'null' => false,
+    'type' => 'int',
+    'length' => '11',
+  ),
+  'subject' => 
+  array (
+    'null' => false,
+    'type' => 'varchar',
+    'length' => '420',
+  ),
+  'template' => 
+  array (
+    'null' => false,
+    'type' => 'varchar',
+    'length' => '180',
+  ),
+  'token' => 
+  array (
+    'null' => false,
+    'type' => 'varchar',
+    'length' => '256',
+  ),
+  'total_sent' => 
+  array (
+    'null' => false,
+    'type' => 'int',
+    'length' => '11',
+  ),
+  'total_viewed' => 
+  array (
+    'null' => false,
+    'type' => 'int',
+    'length' => '11',
+  ),
+  'total_click_through' => 
+  array (
+    'null' => false,
+    'type' => 'int',
+    'length' => '11',
+  ),
+  'created_on' => 
+  array (
+    'null' => false,
+    'type' => 'datetime',
+  ),
+  'variables' => 
+  array (
+    'null' => true,
+    'type' => 'text',
+  ),
+);
 
     /**
-     * Get the required fields of the record. (Wrapper for `self::requiredFields()`).
-     *
-     *
-     * @return array The fields.
-     */
-    public function getRequiredFieldNames(){
-        return self::requiredFieldNames();
-    }//getRequiredFields
-
-
-
-    /**
-    * Register the records validators into `self::$validators`.
-    *
-    *
-    * @return void
+     * @var null|string $autoIncrementField The autoincrement field name.
     */
-    public static function registerValidators(){
-        if(!self::$validators){
-            self::$validators = Array(
-    
-                'id' => \Respect\Validation\Validator::intType()->between(0,99999999999),
-            
-                'subject' => \Respect\Validation\Validator::stringType()->length(0,420),
-            
-                'template' => \Respect\Validation\Validator::stringType()->length(0,180),
-            
-                'token' => \Respect\Validation\Validator::stringType()->length(0,256),
-            
-                'total_sent' => \Respect\Validation\Validator::intType()->between(0,99999999999),
-            
-                'total_viewed' => \Respect\Validation\Validator::intType()->between(0,99999999999),
-            
-                'total_click_through' => \Respect\Validation\Validator::intType()->between(0,99999999999),
-            
-                'created_on' => \Respect\Validation\Validator::date(),
-            
-                'variables' => \Respect\Validation\Validator::alwaysValid(),
-            
-            );
-        }//if
-    }//__registerValidators
+    protected $autoIncrementField = 'id';
 
 
 
-    /**
-    * Determine if a value is valid for a records particular field.
-    *
-    *
-    * @param string $field The field of the record.
-    * @param string $v The value to test against the field.
-    *
-    * @return boolean Did it validate?
-    */
-    public static function validate($field, $v){
-        self::registerValidators();
 
-        if(is_numeric($v)){
-            if(stripos($v,'.') !== false){
-                $v = (float)$v;
-            } else {
-                $v = (int)$v;
-            }//el
-        }//if
-
-        switch($field){
-
-                case 'id' : 
-                    return self::$validators['id']->validate($v) || parent::nullType($v) || parent::rawType($v);
-            
-                case 'subject' : 
-                    return self::$validators['subject']->validate($v) || parent::rawType($v);
-            
-                case 'template' : 
-                    return self::$validators['template']->validate($v) || parent::rawType($v);
-            
-                case 'token' : 
-                    return self::$validators['token']->validate($v) || parent::rawType($v);
-            
-                case 'total_sent' : 
-                    return self::$validators['total_sent']->validate($v) || parent::rawType($v);
-            
-                case 'total_viewed' : 
-                    return self::$validators['total_viewed']->validate($v) || parent::rawType($v);
-            
-                case 'total_click_through' : 
-                    return self::$validators['total_click_through']->validate($v) || parent::rawType($v);
-            
-                case 'created_on' : 
-                    return self::$validators['created_on']->validate($v) || parent::rawType($v);
-            
-                case 'variables' : 
-                    return self::$validators['variables']->validate($v) || parent::nullType($v) || parent::rawType($v);
-            
-                default:
-                    throw new \Disco\exceptions\Record("Record validation exception, record does not have a field `{$field}` to validate against");
-        }//switch
-    }//validate
-
-
-}//Campaign
+}//EnactEmailNewsletterCampaign
--- app/record/CampaignEmail.php
+++ app/record/CampaignEmail.php
@@ -6,137 +6,41 @@ class CampaignEmail extends \Disco\classes\Record {
     protected $model = '\EmailNewsletter\model\CampaignEmail';
     
     
+
     /**
-    * @var Array $validators The validation rules for the fields.
+     * @var null|string $autoIncrementField The autoincrement field name.
     */
-    public static $validators = null;
-
-
-    public static function autoIncrementField(){
-        return false;
-    }//autoIncrementField
-
-
-    /**
-     * Get the fields of the record.
-     *
-     *
-     * @return array The fields.
-     */
-    public static function fieldNames(){
-        return Array(
-    
-                'campaign_id',
-                'email_id',
-                'viewed_on',
-                'clicked_through_on'
-        );
-    }//fields
-
-
-
-    /**
-     * Get the fields of the record. (Wrapper for `self::fields()`).
-     *
-     *
-     * @return array The fields.
-     */
-    public function getFieldNames(){
-        return self::fieldNames();
-    }//getFields
-
-
+    protected $fieldDefinitions = array (
+  'campaign_id' => 
+  array (
+    'null' => false,
+    'type' => 'int',
+    'length' => '11',
+  ),
+  'email_id' => 
+  array (
+    'null' => false,
+    'type' => 'int',
+    'length' => '11',
+  ),
+  'viewed_on' => 
+  array (
+    'null' => true,
+    'type' => 'datetime',
+  ),
+  'clicked_through_on' => 
+  array (
+    'null' => true,
+    'type' => 'datetime',
+  ),
+);
 
     /**
-     * Get the required fields of the record (cannot be NULL).
-     *
-     *
-     * @return array The fields.
-     */
-    public static function requiredFieldNames(){
-        return Array(
-    
-                    'campaign_id',
-                    'email_id'
-        );
-    }//requiredFields
-
-
-
-    /**
-     * Get the required fields of the record. (Wrapper for `self::requiredFields()`).
-     *
-     *
-     * @return array The fields.
-     */
-    public function getRequiredFieldNames(){
-        return self::requiredFieldNames();
-    }//getRequiredFields
-
-
-
-    /**
-    * Register the records validators into `self::$validators`.
-    *
-    *
-    * @return void
+     * @var null|string $autoIncrementField The autoincrement field name.
     */
-    public static function registerValidators(){
-        if(!self::$validators){
-            self::$validators = Array(
-    
-                'campaign_id' => \Respect\Validation\Validator::intType()->between(0,99999999999),
-            
-                'email_id' => \Respect\Validation\Validator::intType()->between(0,99999999999),
-            
-                'viewed_on' => \Respect\Validation\Validator::date(),
-            
-                'clicked_through_on' => \Respect\Validation\Validator::date(),
-            
-            );
-        }//if
-    }//__registerValidators
+    protected $autoIncrementField = false;
 
 
-
-    /**
-    * Determine if a value is valid for a records particular field.
-    *
-    *
-    * @param string $field The field of the record.
-    * @param string $v The value to test against the field.
-    *
-    * @return boolean Did it validate?
-    */
-    public static function validate($field, $v){
-        self::registerValidators();
-
-        if(is_numeric($v)){
-            if(stripos($v,'.') !== false){
-                $v = (float)$v;
-            } else {
-                $v = (int)$v;
-            }//el
-        }//if
-
-        switch($field){
-
-                case 'campaign_id' : 
-                    return self::$validators['campaign_id']->validate($v) || parent::rawType($v);
-            
-                case 'email_id' : 
-                    return self::$validators['email_id']->validate($v) || parent::rawType($v);
-            
-                case 'viewed_on' : 
-                    return self::$validators['viewed_on']->validate($v) || parent::nullType($v) || parent::rawType($v);
-            
-                case 'clicked_through_on' : 
-                    return self::$validators['clicked_through_on']->validate($v) || parent::nullType($v) || parent::rawType($v);
-            
-                default:
-                    throw new \Disco\exceptions\Record("Record validation exception, record does not have a field `{$field}` to validate against");
-        }//switch
-    }//validate
 
 
 }//CampaignEmail
